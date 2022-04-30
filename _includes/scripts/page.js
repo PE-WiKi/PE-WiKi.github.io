24b7088d6c03616f51e6fbdcb60ee753d5ec08a5
JJY <dokleysmanager@gmail.com>
2022-04-30 14:11:37 +0900

    ags

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -1,3 +1,6 @@
 /*(function () {
 
 })();*/
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2482874482799771"
+     crossorigin="anonymous"></script>
